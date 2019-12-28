83415086618793f7e0da1b6f7c40ba835e2c0dca
Maurício Szabo <mauricio.szabo@gmail.com>
2019-12-28 01:19:39 -0300

    Listening to topics!

--- src/microcosmos/kafka.clj
+++ src/microcosmos/kafka.clj
@@ -1,9 +1,85 @@
 (ns microcosmos.kafka
   (:require [jackdaw.client :as jack-client]
-            [jackdaw.client.log :as log]))
+            [jackdaw.client.log :as jack-log]
+            [microcosmos.io :as io]
+            [microcosmos.future :as future]
+            [microcosmos.logging :as log]
+            [clojure.core.async :as async])
+  (:import [org.apache.kafka.clients.consumer OffsetAndMetadata]
+           [org.apache.kafka.common TopicPartition]))
 
 (defn- gen-consumer-config [server group-id]
   {"bootstrap.servers" server
    "group.id" group-id
+   "enable.auto.commit" false
    "key.deserializer" "org.apache.kafka.common.serialization.StringDeserializer"
    "value.deserializer" "org.apache.kafka.common.serialization.StringDeserializer"})
+
+(def ^:private conns (atom {}))
+
+(comment
+  (def opts {:host "localhost:9092" :group-id "grp1"})
+  (def topic-name "test")
+  (with-open [consumer (-> (gen-consumer-config (:host opts) (:group-id opts))
+                           (jack-client/consumer)
+                           (jack-client/subscribe [{:topic-name topic-name}]))]
+    ; (swap! conns assoc topic-name {:consumer consumer
+    ;                                :future (future)})
+    (doseq [val (jack-log/log consumer (:ms-timeout opts 100))]
+      (prn :processing? val))))
+
+
+(defrecord Topic [topic-name opts]
+  io/IO
+  (listen [self function]
+    (let [consumer (-> (gen-consumer-config (:host opts) (:group-id opts))
+                       (jack-client/consumer)
+                       (jack-client/subscribe [{:topic-name topic-name}]))
+          handler (future
+                   (binding [future/execute* future/sync-execute]
+                     (doseq [val (jack-log/log consumer (:ms-timeout opts 100))]
+                       (function {:meta (dissoc val :value)
+                                  :payload (-> val :value io/deserialize-msg)}))))]
+
+      (swap! conns assoc topic-name {:consumer consumer
+                                     :future handler})))
+
+  ; (send! [_ {:keys [payload meta] :or {meta {}}}]
+  ;        (when-not cid (raise-error))
+  ;        (let [payload (io/serialize-msg payload)
+  ;              meta (assoc meta :headers (normalize-headers (assoc meta :cid cid)))]
+  ;          (basic/publish channel name "" payload meta)))
+  ;
+  (ack! [_ {:keys [meta]}]
+    (let [consumer (get-in @conns [topic-name :consumer])
+          topic-partition (new TopicPartition topic-name (:partition meta))
+          offset (new OffsetAndMetadata (-> meta :offset inc))]
+      (.commitSync consumer {topic-partition offset})))
+
+  (log-message [_ logger {:keys [payload meta]}]
+    (let [meta (assoc meta :queue name)]
+      (log/info logger "Processing message"
+                :payload (io/serialize-msg payload)
+                :meta (io/serialize-msg meta)))))
+
+  ; (reject! [self msg _]
+  ;          (let [meta (:meta msg)
+  ;                meta (assoc meta :headers (normalize-headers meta))
+  ;                payload (-> msg :payload io/serialize-msg)]
+  ;            (reject-or-requeue self meta payload))))
+  ;
+  ; health/Healthcheck
+  ; (unhealthy? [_] (when (core/closed? channel)
+  ;                   {:channel "is closed"})))
+  ;
+
+(defn topic [^String topic-name opts]
+  (fn [ & args]
+      (->Topic topic-name opts)))
+
+(defn disconnect-all! []
+  (doseq [[topic-name {:keys [consumer future chan]}] @conns]
+    (.cancel future true)
+    (Thread/sleep 100)
+    (.close consumer)
+    (swap! conns dissoc topic-name)))
--- test/microcosmos/kafka_test.clj
+++ test/microcosmos/kafka_test.clj
@@ -4,32 +4,45 @@
             [microcosmos.kafka :as kafka]
             [microcosmos.future :as future]
             [check.core :refer [check]]
-            [check.async :refer [async-test]]
-            [jackdaw.client :as jack-client]))
+            [check.async :refer [async-test await!]]
+            [jackdaw.client :as jack-client]
+            [clojure.core.async :as async]))
 
 (def producer-config
   {"bootstrap.servers" "localhost:9092"
    "acks" "all"
-   "client.id" "foo"
+   ; "client.id" "foo"
    "key.serializer" "org.apache.kafka.common.serialization.StringSerializer"
    "value.serializer" "org.apache.kafka.common.serialization.StringSerializer"})
 
 (defn- send-message! []
   (with-open [producer (jack-client/producer producer-config)]
-    (jack-client/produce! producer {:topic-name "test"} "{\"hello\":\"world!\"}")))
-
-(defn- deliver-result [promise]
-  (fn [f-msg :keys [output]]
-    (future/intercept (fn [msg]
-                        (deliver promise msg)))))
+    @(jack-client/produce! producer {:topic-name "test"} "{\"hello\":\"world!\"}")))
 
 (deftest kafka-consumer
-  (let [test-topic (kafka/topic "test" {:host "localhost:9092"
-                                        :group-id "grp1"})
-        output-topic (kafka/topic "out" {:host "localhost:9092"
-                                         :group-id "grp1"})
-        sub (components/subscribe-with :test test-topic
-                                       :output output-topic)
-        promise (promise)]
+  (async-test "Consuming kafka messages" {:teardown (kafka/disconnect-all!)}
+    (let [test-topic (kafka/topic "test" {:host "localhost:9092"
+                                          :group-id "grp1"})
+          sub (components/subscribe-with :test test-topic)
+          chan (async/promise-chan)]
 
-    (sub :test (deliver-result promise))))
+      (sub :test (fn [f-msg _] (future/intercept #(async/put! chan %) f-msg)))
+      (send-message!)
+      (check (await! chan) => {:payload {:hello "world!"}}))))
+
+#_
+(deftest kafka-seder
+  (async-test "Consuming kafka, sending to other topics"
+    {:teardown (kafka/disconnect-all!)}
+    (let [test-topic (kafka/topic "test" {:host "localhost:9092" :group-id "grp1"})
+          out (kafka/topic "out" {:host "localhost:9092"})
+          sub (components/subscribe-with :test test-topic)
+          chan (async/promise-chan)]
+
+      (sub :test (deliver-result chan))
+      (send-message!)
+      (check (await! chan) => {:payload {:hello "world!"}}))))
+
+
+#_
+(clojure.test/run-tests)
